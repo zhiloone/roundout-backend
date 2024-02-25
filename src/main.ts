a49fe3227057971618089d4007cdc33bdebadd87
leoofonseca <leonardofonseca.main@gmail.com>
2024-02-25 00:04:45 -0300

    feat: helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { Logger } from '@nestjs/common';
+import helmet from 'helmet';
 
 const logger = new Logger('Main');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
   const port = process.env.PORT || 1234;
-  logger.debug(`API listening on port ${port}`);
   await app.listen(port);
+  logger.log(`API listening on port ${port}`);
 }
 bootstrap();

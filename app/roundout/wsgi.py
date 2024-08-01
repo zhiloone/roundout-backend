ae4c6a8a55ec664754a1f5684f7f653f753ed50e
Leonardo Fonseca Rodrigues <leonardofonseca.main@gmail.com>
2024-08-01 11:44:38 -0300

    feat: pre-commit setup - black

--- app/roundout/wsgi.py
+++ app/roundout/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roundout.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roundout.settings")
 
 application = get_wsgi_application()
